7a284630c6bb400391aee67c9057cd990be47fd2
imadovetch <ibouderoua63@gmail.com>
2024-02-25 18:29:48 +0100

    implement calculator logic

--- src/components/apps/browser.tsx
+++ src/components/apps/browser.tsx
@@ -1,21 +1,51 @@
-import { APP_DATA_TYPE } from "@/data/const"
-import { Window } from './window'
-import { useState } from "react"
+import { APP_DATA_TYPE } from "@/data/const";
+import { Window } from './window';
+import { useState, useRef } from "react";
 
 export function Browser({ data }: { data: APP_DATA_TYPE }) {
+  const [link, setLink] = useState('https://www.imadovetch.tech');
+  const iframeRef = useRef<HTMLIFrameElement>(null);
 
-    const [link, setLink] = useState('')
+  const handleReload = () => {
+    if (iframeRef.current) {
+      iframeRef.current.contentWindow?.location.reload();
+    }
+  };
 
-    return (
-        <Window data={data}>
-            <div className="browser w-full h-full flex flex-col gap-2 p-2">
-                <input
-                    className="bg-transparent w-full ring-2 ring-dark-t rounded-md p-1 px-2"
-                    onChange={(e) => setLink(e.target.value)}
-                />
-                <iframe className="bg-light w-full h-full rounded-md" src={link} />
-            </div>
-        </Window>
-    )
+  const handleGoBack = () => {
+    if (iframeRef.current) {
+      iframeRef.current.contentWindow?.history.back();
+    }
+  };
 
+  return (
+    <Window data={data}>
+      <div className="browser w-full h-full flex flex-col gap-2 p-2">
+        <div className="flex items-center">
+          <input
+            className="bg-transparent flex-grow ring-2 ring-dark-t rounded-md p-1 px-2 focus:outline-none"
+            onChange={(e) => setLink(e.target.value)}
+            value={link}
+          />
+          <button
+            className="ml-2 px-4 py-1 bg-dark-t text-white rounded-md"
+            onClick={() => handleReload()}
+          >
+            Reload
+          </button>
+          <button
+            className="ml-2 px-4 py-1 bg-dark-t text-white rounded-md"
+            onClick={() => handleGoBack()}
+          >
+            Go Back
+          </button>
+        </div>
+        <iframe
+          className="bg-light w-full h-full rounded-md"
+          src={link}
+          ref={iframeRef}
+        />
+      </div>
+    </Window>
+  );
 }
--- src/components/apps/calculator.tsx
+++ src/components/apps/calculator.tsx
@@ -1,38 +1,83 @@
-'use client'
+'use client';
 
-import { APP_DATA_TYPE } from "@/data/const"
-import { Window } from './window'
+import { APP_DATA_TYPE } from "@/data/const";
+import { Window } from './window';
+import { useState } from 'react';
 
 export function Calculator({ data }: { data: APP_DATA_TYPE }) {
+  const KEYS_LIST = [
+    '7',
+    '8',
+    '9',
+    '+',
+    '4',
+    '5',
+    '6',
+    '-',
+    '1',
+    '2',
+    '3',
+    'x',
+    '/',
+    'DEL',
+    '0',
+    '.',
+    'C',
+    '=',
+  ];
 
-    const KEYS_LIST = [
-        '7',
-        '8',
-        '9',
-        '+',
-        '4',
-        '5',
-        '6',
-        '-',
-        '1',
-        '2',
-        '3',
-        'x',
-        'DEL',
-        '0',
-        '.',
-        '=',
-    ]
+  const [result, setResult] = useState<string>('');
 
-    return (
-        <Window data={data}>
-            <div className="w-full h-full flex flex-col gap-2 p-2">
-                <div className="bg-dark-t p-2 rounded-md min-h-[100px] h-full">Result</div>
-                <div className="grid grid-cols-4 gap-2">
-                    {KEYS_LIST.map(key => <button className="btn-dark">{key}</button>)}
-                </div>
-            </div>
-        </Window>
-    )
+  const handleButtonClick = (key: string) => {
+    switch (key) {
+      case '=':
+        calculateResult();
+        break;
+      case 'DEL':
+        deleteLastChar();
+        break;
+      case 'C':
+        clearResult();
+        break;
+      default:
+        setResult((prevResult) => prevResult + key);
+    }
+  };
 
+  const calculateResult = () => {
+    try {
+      const expression = result.replace(/x/g, '*').replace(/÷/g, '/');
+      setResult(eval(expression).toString());
+    } catch (error) {
+      setResult('Error');
+    }
+  };
+
+  const deleteLastChar = () => {
+    setResult((prevResult) => prevResult.slice(0, -1));
+  };
+
+  const clearResult = () => {
+    setResult('');
+  };
+
+  return (
+    <Window data={data}>
+      <div className="w-full h-full flex flex-col gap-2 p-2">
+        <div className="bg-dark-t p-2 rounded-md min-h-[100px] h-full">{result}</div>
+        <div className="grid grid-cols-4 gap-2">
+        {KEYS_LIST.map((key) => (
+            <button
+                key={key}
+                className={`btn-dark ${key === 'C' ? 'col-span-3' : ''}`}
+                onClick={() => handleButtonClick(key)}
+            >
+                {key}
+            </button>
+        ))}
+
+        </div>
+      </div>
+    </Window>
+  );
 }
