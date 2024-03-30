ee7ef11fa9f952013ae70ac316b88ddaa910cbef
imadovetch <ibouderoua63@gmail.com>
2024-03-30 15:17:37 +0000

    test localhost

--- src/data/const.ts
+++ src/data/const.ts
@@ -1,7 +1,7 @@
 // Constants 
 
 export const WALLPAPER_LINK = "/wallpaperflare.com_wallpaper.jpg"
-export const BACKEND_LINK = "https://full-os-back-end.vercel.app/"
+export const BACKEND_LINK = "http://localhost:8000/"
 
 // Available applications
 export const AVAILABLE_APPS = ['manager', 'calculator', 'camera', 'game', 'browser','chat','store','weather','yourlink']
