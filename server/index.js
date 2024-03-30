ef35d044a76185ae68a55e0f6d4517f5fe882f6b
imadovetch <ibouderoua63@gmail.com>
2024-03-30 21:28:16 +0000

    CORS json update

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const { Server } = require('socket.io');
 const server = http.createServer();
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000", // Allow requests from this origin
+    origin: "*", // Allow requests from this origin
     methods: ["GET", "POST"] // Allow only GET and POST requests
   }
 });
