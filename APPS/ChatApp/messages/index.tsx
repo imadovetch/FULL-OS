422be5d5a32101cc9b4f47341513bd017bf25061
imadovetch <ibouderoua63@gmail.com>
2024-03-31 17:58:48 +0000

    push from desktop

--- APPS/ChatApp/messages/index.tsx
+++ APPS/ChatApp/messages/index.tsx
@@ -25,7 +25,7 @@ export default function Messages({owner ,data, totext }) {
   }
 
   useEffect(() => {
-      const newSocket = io('https://full-os.vercel.app:3001');
+      const newSocket = io('http://localhost:3001');
       setSocket(newSocket);
 
       return () => {
--- src/data/const.ts
+++ src/data/const.ts
@@ -2,7 +2,7 @@
 
 export const WALLPAPER_LINK = "/wallpaperflare.com_wallpaper.jpg"
 export const BACKEND_LINK = "http://localhost:8000/"
-
+const test = 1
 // Available applications
 export const AVAILABLE_APPS = ['manager', 'calculator', 'camera', 'game', 'browser','chat','store','weather','yourlink']
 export type APP_DATA_TYPE = {
