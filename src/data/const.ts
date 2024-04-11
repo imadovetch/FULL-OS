47da24d44ffd2ba58134226aa7240ecd7d5f614e
imadovetch <ibouderoua63@gmail.com>
2024-04-11 19:44:22 +0000

    all 3 dep are working bug

--- src/data/const.ts
+++ src/data/const.ts
@@ -1,7 +1,7 @@
 // Constants 
 
 export const WALLPAPER_LINK = "/wallpaperflare.com_wallpaper.jpg"
-export const BACKEND_LINK = "https://full-os-backhosted-production.up.railway.app/"
+export const BACKEND_LINK = "http://localhost:8000/"
 
 // Available applications
 export const AVAILABLE_APPS = ['manager', 'calculator', 'camera', 'game', 'browser','chat','store','weather','yourlink','Settings','Gallery','Yomahoot']
