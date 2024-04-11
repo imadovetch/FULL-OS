6b80d703667520a5603ef7e711fb19871a5155f6
imadovetch <ibouderoua63@gmail.com>
2024-04-11 21:06:03 +0000

    everything is good  added token chack in home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,15 @@
 "use client"
-import { useRouter } from 'next/navigation';
+
 import { Provider } from "react-redux"
 import { store } from "@/data/store"
 import { useGSAP } from "@gsap/react"
 import { useRef } from "react"
 import gsap from "gsap"
 import { Wallpaper, Header, Apps , Widgets ,Shortcuts} from "@/components"
-import { gettoken  } from "../../utils/modules"
-export default function Home() {
-  const router = useRouter();
-  const main = useRef(null)
-  const token = gettoken('token');
-  if(!token) router.push('/Landing');
 
+export default function Home() {
+
+  const main = useRef(null)
 
   useGSAP(() => {
 
